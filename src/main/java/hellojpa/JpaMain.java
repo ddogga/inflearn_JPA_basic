f07d7f80fba28629cf4e237be93a48341ec34001
ddogga <hayeonlee60327@gmail.com>
2023-01-05 22:29:22 +0900

    실습 - 회원 수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,7 +9,7 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
+        // 클라이언트에서 데이터 변경 요청이 들어 올때 마다 생성되고 소멸됨
         EntityManager em = emf.createEntityManager();
 
 
@@ -17,12 +17,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-
-            member.setId(1L);
-            member.setName("HelloA");
-
-            em.persist(member);
+            // 회원 수정
+            // JPA로 찾은 객체는 JPA가 관리하므로 변경을 감지
+            Member findMember = em.find(Member.class, 1L);
+            // set함수로 변경한 것을 감지하여 db에 update 쿼리를 날림
+            findMember.setName("HelloJPA");
 
             tx.commit();
         } catch (Exception e) {
