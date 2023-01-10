cbba59dfb0ad959cd4ff34778f96fb8f415f5591
ddogga <hayeonlee60327@gmail.com>
2023-01-10 22:10:34 +0900

    연관관계 매핑 - 양방향 연관관계 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -29,13 +30,17 @@ public class JpaMain {
             member.setTeam(team);//단방향 연관관계 설정, 참조 저장
             em.persist(member);
 
-            //조회
+            //쿼리 바로 실행
+            em.flush();
+            em.clear();
+
+            //Team에서 member 조회
             Member findMember = em.find(Member.class, member.getId());
-
-            //참조를 사용해서 연관관계 조회
-            Team findTeam = findMember.getTeam();
-
-            System.out.println("findTeam = " + findTeam);
+            List<Member> members = findMember.getTeam().getMembers();
+            
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
             System.out.println("===================================================");
             tx.commit();
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,15 +1,27 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
 
     @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 
     public Long getId() {
         return id;
@@ -26,4 +38,6 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
