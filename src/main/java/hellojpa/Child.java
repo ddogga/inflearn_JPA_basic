7152a50649379a60815ccd16f3d0742c572285e9
ddogga <hayeonlee60327@gmail.com>
2023-01-17 21:40:30 +0900

    영속성전이 CASCADE - 영속성 전이가 필요한 경우(수동으로 persist해주는 예제)

---
+++ src/main/java/hellojpa/Child.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Child {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,33 +20,17 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Team teamB = new Team();
-            teamB.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-//            Member m = em.find(Member.class,member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
-
-//            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
 
             tx.commit();
