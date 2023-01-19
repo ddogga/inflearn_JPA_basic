b88138f63c67878311e9df1fb35ecbaf34c7a291
ddogga <hayeonlee60327@gmail.com>
2023-01-19 09:36:55 +0900

    값 타입 - 값 타입 컬렉션 사용예제(저장)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,7 +20,18 @@ public class JpaMain {
 
         try {
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity","street","1000"));
 
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1","street","1000"));
+            member.getAddressHistory().add(new Address("old2","street","1000"));
+
+            em.persist(member);
 
             tx.commit();
 
