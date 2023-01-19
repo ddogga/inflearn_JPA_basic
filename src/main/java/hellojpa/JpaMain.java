d17da5c80f222488f653863f88479a750b0c49b0
ddogga <hayeonlee60327@gmail.com>
2023-01-19 09:46:55 +0900

    값 타입 - 값 타입 컬렉션 사용예제(수정)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -32,6 +33,22 @@ public class JpaMain {
             member.getAddressHistory().add(new Address("old2","street","1000"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("============================= START =============================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address.getCity() = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
 
             tx.commit();
 
